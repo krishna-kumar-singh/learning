1332ddfccc83c93482c8085d8f7a4cd0f155d2b0
kkrishna09 <krishnarajput00009@gmail.com>
2024-01-28 09:08:22 +0530

    whitespaces

--- megaBlog/src/component/header/Header.jsx
+++ megaBlog/src/component/header/Header.jsx
@@ -18,22 +18,22 @@ function Header() {
       name: "Login",
       slug: "/login",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "Signup",
       slug: "/signup",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "All Posts",
       slug: "/all-posts",
       active: authStatus,
-  },
-  {
+    },
+    {
       name: "Add Post",
       slug: "/add-post",
       active: authStatus,
-  },
+    },
   ]
   return (
     <header className=' py-3 shadow bg-gray-500'>
