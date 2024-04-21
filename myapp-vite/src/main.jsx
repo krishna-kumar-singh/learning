7b84555bb2addf8377f1c7f5dbaa802c816f813d
Krishna <krishnarajput00009@gmail.com>
2024-04-21 22:02:55 +0530

    main file

--- myapp-vite/src/main.jsx
+++ myapp-vite/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// g
+// app file
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
