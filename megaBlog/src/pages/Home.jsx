aea1da57d0bc6885e917b4d4719b252272bb6b55
Krishna <krishnarajput00009@gmail.com>
2024-04-24 18:54:56 +0530

    home and post route

--- megaBlog/src/pages/Home.jsx
+++ megaBlog/src/pages/Home.jsx
@@ -1,46 +1,45 @@
-import React, {useEffect,useState} from 'react'
-import service from '../appwrite/config'
-import { Container,PostCard } from '../component'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import service from '../appwrite/config';
+import { PostCard } from '../component';
 
 function Home() {
-    const [posts,setPosts]=useState([])
-    useEffect(()=>{
-        service.getPosts().then((value)=> {
-            if (value){
-                setPosts(value.documents)
-            }
-        })
-    },[])
-    
-  if (posts.length === 0){
-    return (
-      <div className="w-full py-8 mt-4 text-center">
-          <Container>
-              <div className="flex flex-wrap">
-                  <div className="p-2 w-full">
-                      <h1 className="text-2xl font-bold hover:text-gray-500">
-                          Login to read posts
-                      </h1>
-                  </div>
-              </div>
-          </Container>
-      </div>
-    )}
-    return (
-      <div className='w-full py-8'>
-      <Container>
-          <div className='flex flex-wrap'>
-              {posts.map((post) => (
-                  <div key={post.$id} className='p-2 w-1/4'>
-                      <PostCard {...post} />
-                  </div>
-              ))}
-          </div>
-      </Container>
-  </div>
+    const userData = useSelector((state) => state.auth.userData);
+    const [posts, setPosts] = useState([]);
 
-  )
-  
+    useEffect(() => {
+        if (userData) {
+            service.getPosts().then((value) => {
+                if (value) {
+                    setPosts(value.documents);
+                }
+            });
+        }
+    }, [userData]);
+
+    if (!userData || posts.length === 0) {
+        return (
+            <div className="w-full h-[500px] flex items-center justify-center">
+                <div className="text-center">
+                    <h1 className="text-3xl font-bold mb-4">Welcome to Tech-Trends</h1>
+                    <p className="text-lg mb-8">Stay up to date with the latest tech trends and insights!</p>
+                    <p className="text-lg">To explore more, please log in and start sharing your tech knowledge.</p>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="w-full py-8">
+            <div className="flex flex-wrap">
+                {posts.map((post) => (
+                    <div key={post.$id} className="p-2 w-1/4">
+                        <PostCard {...post} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
--- megaBlog/src/pages/Post.jsx
+++ megaBlog/src/pages/Post.jsx
@@ -33,35 +33,36 @@ export default function Post() {
     };
 
     return post ? (
-        <div className="py-8">
-            <Container>
-                <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+        <div className=" flex flex-col  mx-auto w-4/5  my-12 border-gray-500 shadow-md  border rounded-md h-[500px]">
+           
+                <div className="w-auto flex justify-center mb-4 relative  rounded-xl p-2 bg-white">
                     <img
+
                         src={service.getFilePreview(post.featuredImage)}
                         alt={post.title}
-                        className="rounded-xl"
+                        className="rounded-xl mt-2 aspect-auto max-h-[300px] max-w-[500px] object-contain"
                     />
 
                     {isAuthor && (
                         <div className="absolute right-6 top-6">
-                            <Link to={`/edit-post/${post.$id}`}>
-                                <Button bgColor="bg-green-500" className="mr-3">
-                                    Edit
-                                </Button>
-                            </Link>
-                            <Button bgColor="bg-red-500" onClick={deletePost}>
+                            
+                                <button className="mr-3 min-w-[86px] px-5 py-2 rounded-md bg-blue-500 text-white hover:bg-white hover:text-blue-500 hover:border hover:border-blue-500 duration-200 ">
+                                <Link to={`/edit-post/${post.$id}`}> Edit</Link>
+                                </button>
+                            
+                            <button onClick={deletePost} className="mr-3 min-w-[86px] px-5 py-2 rounded-md bg-red-500 text-white hover:bg-white hover:text-red-500 hover:border hover:border-red-500 duration-200 ">
                                 Delete
-                            </Button>
+                            </button>
                         </div>
                     )}
                 </div>
-                <div className="w-full mb-6">
-                    <h1 className="text-2xl font-bold">{post.title}</h1>
+                <div className="w-auto mb-6  pl-5 border-gray-500 border-b-2">
+                    <h1 className="text-4xl font-bold text-gray-600">Title {post.title}</h1>
                 </div>
-                <div className="browser-css">
+                <div className="text-lg text-gray-700 pl-5">
                     {parse(post.content)}
-                    </div>
-            </Container>
+                </div>
+
         </div>
-    ) : null;
+    ) : <div className="bg-red">No post Available</div>;
 }
