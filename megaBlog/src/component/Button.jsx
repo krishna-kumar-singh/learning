ada80b5fad5f88bc39dc94b106a0218650c1cde1
Krishna <krishnarajput00009@gmail.com>
2024-04-24 18:52:08 +0530

    basic component

--- megaBlog/src/component/Button.jsx
+++ megaBlog/src/component/Button.jsx
@@ -9,7 +9,7 @@ function Button({
     ...props
 }){
   return (
-    <button className={` ${className} ${textColor} px-6 py-2 duration-200 hover: bg-blue-100 rounded-full  `} type={type}
+    <button className={` ${className} ${bgColor} text-${textColor}  text-center  py-2 duration-200 hover: bg-blue-100 rounded-full  `} type={type}
     {...props} >{children}</button>
   )
 }
--- megaBlog/src/component/Input.jsx
+++ megaBlog/src/component/Input.jsx
@@ -40,7 +40,7 @@ const Input = React.forwardRef( function Input({
     return (
         <div className='w-full'>
             {label && <label 
-            className='inline-block mb-1 pl-1' 
+            className='inline-block text-lg mb-1 pl-1' 
             htmlFor={id}>
                 {label}
             </label>
